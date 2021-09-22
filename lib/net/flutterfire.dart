7b30a7bca600e1b139ebdaa2592f9d35ce75d9de
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-09-22 23:02:33 +0200

    Ajout de cryptomonnaie

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -1,5 +1,6 @@
 
 // ignore: import_of_legacy_library_into_null_safe
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<bool> signIn(String email, String password) async {
@@ -29,6 +30,27 @@ Future<bool> register(String email, String password) async {
   }catch(e){
     // ignore: avoid_print
     print(e.toString());
+    return false;
+  }
+}
+
+Future<bool> addCoin(String id, String amount) async{
+  try{
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    var value = double.parse(amount);
+    DocumentReference documentReference = FirebaseFirestore.instance.collection('Users').doc(uid).collection('Coins').doc(id);
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(documentReference);
+      if(!snapshot.exists){
+        documentReference.set({'Amount':value});
+        return true;
+      }
+      double newAmount = snapshot.data()['Amount'] + value;
+      transaction.update(documentReference, {'Amount':newAmount});
+      return true;
+    });
+    return true;
+  }catch(e){
     return false;
   }
 }
